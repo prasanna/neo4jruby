306c1dccdc862def7cb3b57c0a46f2c541323b11
Prasanna Pendse <pendsepr@ppendse.(none)>
2009-10-31 23:13:47 +0530

    Basic traversal example

--- lib/init.rb
+++ lib/init.rb
@@ -5,3 +5,7 @@ Dir["vendor/lib/*.jar"].each { |jar| require jar }
 
 import org.neo4j.api.core.EmbeddedNeo
 import org.neo4j.api.core.RelationshipType
+import org.neo4j.api.core.Traverser
+import org.neo4j.api.core.StopEvaluator
+import org.neo4j.api.core.ReturnableEvaluator
+import org.neo4j.api.core.Direction
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -13,17 +13,49 @@ describe "neo" do
     begin
       neo = EmbeddedNeo.new("var/neo")
       tx = neo.beginTx
-      firstNode = neo.createNode
-      secondNode = neo.createNode
-      relationship = firstNode.createRelationshipTo( secondNode, KNOWS = MyRelationshipType.new("KNOWS"))
       
-      firstNode.setProperty( "message", "Hello, " )
-      secondNode.setProperty( "message", "world!" )
-      relationship.setProperty( "message", "brave Neo " )
+      mr_anderson = neo.create_node
+      mr_anderson.set_property "name", "Thomas Anderson"
+      mr_anderson.set_property "age", 29
       
-      puts firstNode.getProperty( "message" )
-      puts relationship.getProperty("message")
-      puts secondNode.getProperty("message")
+      morpheus = neo.create_node
+      morpheus.set_property "name", "Morpheus"
+      morpheus.set_property "rank", "Captain"
+      morpheus.set_property "occupation", "Total bad ass"
+      
+      trinity = neo.create_node
+      trinity.set_property "name", "Trinity"
+      
+      cypher = neo.create_node
+      cypher.set_property "name", "cypher"
+      
+      agent_smith = neo.create_node
+      agent_smith.set_property "name", "agent_smith"
+      
+      architect = neo.create_node
+      architect.set_property "name", "architect"
+
+      KNOWS = MyRelationshipType.new("KNOWS")
+      CODED_BY = MyRelationshipType.new("CODED_BY")
+
+      relationship = mr_anderson.create_relationship_to( morpheus, KNOWS)
+      mr_anderson.create_relationship_to(trinity, KNOWS)
+      morpheus.create_relationship_to(trinity, KNOWS)
+      morpheus.create_relationship_to(cypher, KNOWS)
+      cypher.create_relationship_to(agent_smith, KNOWS)
+      agent_smith.create_relationship_to(architect, CODED_BY)
+      
+      friends_traverser = mr_anderson.traverse( 
+        Traverser::Order::BREADTH_FIRST, 
+        StopEvaluator::END_OF_NETWORK, 
+        ReturnableEvaluator::ALL_BUT_START_NODE, 
+        KNOWS, 
+        Direction::OUTGOING )
+        
+      friends_traverser.each do |friend|
+        puts "Depth: #{friends_traverser.current_position.depth}"
+        puts "Name: #{friend.get_property('name')}"
+      end
       
       tx.success
     ensure
